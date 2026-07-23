922c2457026f75188e1b942a24e9bbb21b94ca45
tharunkumaran05-ship-it <tharunkumaran05@gmail.com>
2026-07-23 14:24:29 +0530

    Added Prometheus metrics

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.models import SMSRequest
 from app.sms_service import send_sms
 from app.database import create_tables, get_sms_history
 from app.security import verify_api_key
+from prometheus_fastapi_instrumentator import Instrumentator
 
 create_tables()
 
@@ -12,20 +13,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
-
 @app.get("/")
 def home():
     return {
         "project": "SMSBridge Version 2"
     }
 
-
 @app.get("/health")
 def health():
     return {
         "status": "Running"
     }
-
 
 @app.post("/send-sms")
 def send_sms_api(
@@ -37,9 +35,10 @@ def send_sms_api(
         request.message
     )
 
-
 @app.get("/sms-history")
 def sms_history(
     api_key: str = Depends(verify_api_key)
 ):
     return get_sms_history()
+
+Instrumentator().instrument(app).expose(app)
